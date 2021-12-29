bb82a5f16568fc1d071dfaa58a0fd99c428c4e8b
mmazurky <mmazurky@gmail.com>
2021-12-29 11:29:56 -0300

    Adding comments to the code

--- nodejs-functions/save-scraping-result-to-db/index.js
+++ nodejs-functions/save-scraping-result-to-db/index.js
@@ -4,35 +4,39 @@ const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+// starts the db library 
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+        database: dbName,
+        host: dbHost,
+        user: dbUser,
+        password: dbPassword
+    }
+});
+
 exports.handler = function(event, context, callback) {
     console.log('Received event:', event);
 
     try {
+        // saves the result to the DB
         saveToDB(event);
+        // returns the request status
         executeCallback(null, callback);
     } catch (error) {
         console.log("An error occurred: " + error);
+        // returns the request status
         executeCallback(error, callback);
     }
 };
 
 function saveToDB(scrapingData) {
-    const options = {
-        client: 'mysql2',
-        connection: {
-            database: dbName,
-            host: dbHost,
-            user: dbUser,
-            password: dbPassword
-        }
-    }
-
-    const knex = require('knex')(options);
-
+    // uses the column 'scraping_data' to store the result
     const scrapingResult = [
         { scraping_data: scrapingData }
     ]
 
+    // inserts the result in tb_scraping table
     knex('tb_scraping').insert(scrapingResult).then(() => console.log("data inserted"))
         .catch((err) => { console.log(err); throw err })
         .finally(() => {
@@ -46,5 +50,6 @@ function executeCallback(error, callback) {
         success : !error
     };
 
+    // executes the callback
     callback(error, JSON.stringify(response));
 }
