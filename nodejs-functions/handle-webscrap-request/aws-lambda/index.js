74f8d363fe2e0974ff2a06f16d900b5c3dd950ff
mmazurky <mmazurky@gmail.com>
2022-01-03 12:22:11 -0300

    Refactoring module

---
+++ nodejs-functions/handle-webscrap-request/aws-lambda/index.js
@@ -0,0 +1,45 @@
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
+const awsLambdaUtilities = require('../lib/aws-lambda-utilities');
+const https = require('https');
+const http = require('http');
+
+const handler = function(event, context) {
+    try {
+        var body = awsLambdaUtilities.getBodyJson(event);
+        var url = event.url && event.url != null ? event.url : body != null && body.url ? body.url : null;
+        var name = event.name && event.name != null ? event.name : body != null && body.name ? body.name : null;
+        var selector = event.selector && event.selector != null ? event.selector : body != null && body.selector ? body.selector : null;
+
+        handleWebscrapRequest(url, name, selector).then(() => {
+            console.log("Finished with success!");
+            context.succeed();
+        }).catch(error => {
+            console.log("An exception has occurred: " + error);
+            context.fail(error);    
+        });
+    }
+    catch (error) {
+        console.log("An exception has occurred: " + error);
+        context.fail(error);
+    }
+};
+
+function handleWebscrapRequest(url, name, selector) {
+    return new Promise((resolve, reject) => {
+        try {
+            awsLambdaUtilities.callLambdaFunction('create-sitemap-request', { "name": name, "url": url, "selector" : selector }).then(payload => {
+                awsLambdaUtilities.callLambdaFunction('create-scraping-job-request', { "sitemapId": awsLambdaUtilities.getJsonPayload(payload).sitemapId }).then(() => {
+                    resolve(true);
+                }).catch(e => reject(e));
+            }).catch(e => reject(e));
+        }
+        catch (error) {
+            reject(error);
+        }
+    });
+}
+
+module.exports = {
+    handler
+}
---
+++ nodejs-functions/handle-webscrap-request/lib/aws-lambda-utilities.js
@@ -0,0 +1,57 @@
+const executeCallback = function(error, callback) {
+    let response = {
+        status: !error ? 200 : 400,
+        success: !error
+    };
+
+    // executes the callback
+    callback(null, JSON.stringify(response));
+};
+
+const getBodyJson = function(event) {
+    try {
+        return event.body && event.body != null ? JSON.parse(event.body) : null;
+    }
+    catch (e) {
+        console.log("Exception: " + e);
+    }
+};
+
+const getJsonPayload = function(data) {
+    let hasPayload = data && data.Payload;
+    return hasPayload ? JSON.parse(data.Payload) : null;
+};
+
+const callLambdaFunction = function(functionName, payload) {
+    // calls another lambda function
+    return new Promise((resolve, reject) => {
+        var params = {
+            FunctionName: functionName,
+            InvocationType: 'RequestResponse',
+            LogType: 'Tail',
+            Payload: JSON.stringify(payload)
+        };
+
+
+        lambda.invoke(params, function(err, data) {
+            let hasPayload = data && data.Payload;
+            let jsonPayload = hasPayload ? JSON.parse(data.Payload) : null;
+            
+            let success = hasPayload ? JSON.parse(jsonPayload).success : false;
+
+            if (!success || err) {
+                console.log("Error calling the Lambda Function " + functionName + ": ", data ? data : err);
+                reject("Function:" + functionName);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+module.exports = {
+    executeCallback,
+    getBodyJson,
+    getJsonPayload,
+    callLambdaFunction
+};
